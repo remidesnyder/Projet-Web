fac2b0f30d526c348c284610ea23dfab3669bfc8
_Dup62 <52581851+dup62@users.noreply.github.com>
2023-04-14 20:13:27 +0200

    Update config.php

--- libs/config.php
+++ libs/config.php
@@ -1,19 +1,9 @@
 <?php
 
-/*
-IMPORTANT : pour créer de nouvelles bases sur les installations de mysql sur les postes de l'ig2i, il faut nommer les bases 'testdb_<nom>', et utiliser le fichier de configuration suivant :
-
-$BDD_host="localhost;port=3307";
-$BDD_user="administrateur";
-$BDD_password=""; // pas de mot de passe
-$BDD_base="testdb_<nom>";
-*/
-
-
 // MACHINE LINUX 
 $BDD_host="localhost";
 $BDD_user="root";
-$BDD_password="mysql"; // vide sous windows
-$BDD_base="2iMVC";
+$BDD_password="root"; // vide sous windows
+$BDD_base="db_projet_web";
 
 ?>
