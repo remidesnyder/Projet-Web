be1239c9cf0f49a45e8337deb1a11541d7e679eb
Charly <cav.photos@orange.fr>
2023-05-22 20:23:38 +0200

    JS Updates
    - (Tried to) add "popups" when the user hover an object with his mouse. Doesn't work ATM, needs to be repaired.

--- public/js/index.js
+++ public/js/index.js
@@ -192,3 +192,48 @@ var swiperActorMovies = new Swiper(".actorMovies-content", {
         },
     },
 });
+
+
+var refPopup = null;
+
+function init() {
+    console.log("init");
+    refPopup = document.getElementById("popup");
+}
+
+var data = {
+    home: "Revenir à l'accueil",
+    pop: "Voir les films populaires",
+    fav: "Voir les films favoris",
+};
+
+function popin(contexte) {
+    console.log("popin" + contexte);
+    console.log(contexte.target.id);
+    console.log("clientX" + contexte.clientX); 
+    console.log("screenX" + contexte.screenX); 
+    observateur = contexte.target;
+
+    if (data[contexte.target.id] == undefined) return;
+    refPopup.innerHTML = data[contexte.target.id];
+
+    refPopup.style.top = parseInt(contexte.clientY) + 10 + "px";
+
+   
+    if (contexte.clientX <= window.innerWidth / 2) {
+        refPopup.style.left = parseInt(contexte.clientX) + 5 + "px";
+        refPopup.style.right = "";
+    } else {
+        refPopup.style.right =
+            window.innerWidth - parseInt(contexte.clientX) + 5 + "px";
+        refPopup.style.removeProperty("left");
+    }
+
+
+    refPopup.style.display = "block";
+}
+
+function popout() {
+    console.log("popout");
+    refPopup.style.display = "none";
+}
