80a115c54973faefe3adfca16269852310392366
Charly <66905063+EnisuVI@users.noreply.github.com>
2023-05-22 15:25:03 +0200

    Signup/Login
    - Modifications in the login and signup pages
    - Modifications of the CSS for those two

--- templates/signup.php
+++ templates/signup.php
@@ -12,15 +12,21 @@ $error = valider("error");
 <section></section>
 <section>
     <h1 class="signup">Inscription</h1>
+    <br />
     <?php if ($error) { ?>
         <div class="error" style="color: red;"><?= $error ?></div>
     <?php } ?>
     <div id="formSignup" class="signup">
         <form action="controleur.php" method="POST">
-            <label for="login"> Login : </label><input type="text" id="login" name="login"/><br />
-            <label for="passe">Passe : </label><input type="password" id="passe" name="passe"/><br />
+            <input type="text" id="login" placeholder="Nom d'utilisateur" name="login"/><br />
+            <input type="password" id="passe" placeholder="Mot de passe" name="passe"/><br />
 
             <input type="submit" name="action" value="Inscription" />
         </form>
+    </div>
+    <br />
+    <div class="comptePasCompte">
+    <h4 class="comptePasCompte">Déjà inscrit ?</h4>
+    <a class="comptePasCompte" href="index.php?view=login">Se connecter</a>
     </div>
 </section>
